612ea99270a04d1da96d2482950b5d8ff97fd00f
Pirate298 <pirate@PIRATEs-MacBook-Pro.local>
2016-10-26 09:55:31 +0700

    Login

--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tf_pass: UITextField!
+    @IBOutlet weak var tf_user: UITextField!
+    
+    @IBAction func act_dangnhap(_ sender: AnyObject) {
+        if let pass = user[tf_user.text!]
+        {
+            if pass == tf_pass.text
+            {
+            print("dang nhap thanh cong")
+                let tabBar = self.storyboard?.instantiateViewController(withIdentifier: "tabBar")
+                self.navigationController?.pushViewController(tabBar!, animated: true)
+            }
+            else
+            {
+                print("mat khau khong dung")
+            }
+        }
+        else{
+            print(("tai khoan khong ton tai"))
+        }
+    }
+    
+    var user = ["duy":"123", "hiep" : "345" , "cong" : "456"]
+    
+    
+    @IBAction func btn_dsnd(_ sender: AnyObject) {
+    for (tendangnhap , matkhau) in user
+    {
+        print("Ten dang nhap la : \(tendangnhap)  Mat khau la : \(matkhau)")
+        //print("Mat khau la : \(matkhau)")
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        user ["tung"] = "234"
+       // let values = user.values
+        let key = user.keys
+        for key in key
+        {
+            print(key)
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
